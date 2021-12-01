4f3d74753e5e0430378f88683e3c4d5b985b3c56
Armando Partida-Sanabia <armandopartida0@outlook.com>
2021-12-01 00:34:57 -0500

    Initial day-1 code

---
+++ day-1/main.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file main.cpp Day 1 Solution
+ * @author Armando Partida-Sanabia
+ * @brief Sonar Sweep
+ * @date 2021-12-01
+ * 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    
+}
